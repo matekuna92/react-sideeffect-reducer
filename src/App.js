bcdc674ae0bfd3c94fb6c8d3f81e7981765663e1
matekuna92 <kunamate66@gmail.com>
2022-12-22 15:23:32 +0100

    useEffect with localStorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const isUserLoggedIn = localStorage.getItem('isLoggedIn');
-
-  if(isUserLoggedIn === '1') {
-    setIsLoggedIn(true);
-  }
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
@@ -19,6 +13,18 @@ function App() {
     localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
+
+  // function is executed after every component re-evaluation,
+  // it runs when the dependencies changed in the useEffect dependency array
+  // dependencies change on first run (when app loads), but since they dont change after (array is empty),
+  // this useEffect function only run once here
+  useEffect(() => {
+      const isUserLoggedIn = localStorage.getItem('isLoggedIn');
+
+      if(isUserLoggedIn === '1') {
+        setIsLoggedIn(true);
+      }
+  }, []);
 
   const logoutHandler = () => {
     localStorage.setItem('isLoggedIn', '0');
